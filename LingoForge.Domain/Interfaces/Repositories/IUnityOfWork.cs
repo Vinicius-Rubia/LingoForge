a46deb22e78d03b824edbe38c0a868f82e2d1ff7
Vinicius-Rubia <lucas.de.oliveira.rubia1@gmail.com>
2025-07-23 20:28:54 -0300

    feat: adicionar padrão de unidade de trabalho - UnityOfWork

---
+++ LingoForge.Domain/Interfaces/Repositories/IUnityOfWork.cs
@@ -0,0 +1,6 @@
+﻿namespace LingoForge.Domain.Interfaces.Repositories;
+
+public interface IUnityOfWork
+{
+    Task Commit();
+}
---
+++ LingoForge.Infrastructure/DataAccess/Repositories/UnityOfWork.cs
@@ -0,0 +1,10 @@
+﻿using LingoForge.Domain.Interfaces.Repositories;
+
+namespace LingoForge.Infrastructure.DataAccess.Repositories;
+
+internal class UnityOfWork(LingoForgeDbContext dbContext) : IUnityOfWork
+{
+    private readonly LingoForgeDbContext _dbContext = dbContext;
+
+    public async Task Commit() => await _dbContext.SaveChangesAsync();
+}
