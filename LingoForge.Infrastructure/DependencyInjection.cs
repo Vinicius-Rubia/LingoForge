d85fa9aefebb29b2afdd76352378e1f71ad4962c
Vinicius-Rubia <lucas.de.oliveira.rubia1@gmail.com>
2025-07-23 20:31:00 -0300

    feat: criar policies para lidar com o tipo de usuário e adicionar arquivos de injeção de dependências

---
+++ LingoForge.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,38 @@
+﻿using LingoForge.Domain.Interfaces.Repositories;
+using LingoForge.Domain.Security.Tokens;
+using LingoForge.Infrastructure.DataAccess;
+using LingoForge.Infrastructure.DataAccess.Repositories;
+using LingoForge.Infrastructure.Security.Tokens;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LingoForge.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        AddDBContext(services, configuration);
+        AddRepositories(services);
+        AddToken(services, configuration);
+    }
+
+    public static void AddToken(IServiceCollection services, IConfiguration configuration)
+    {
+        var expirationTimeMinutes = configuration.GetValue<uint>("Settings:Jwt:ExpiresMinutes");
+        var signingKey = configuration.GetValue<string>("Settings:Jwt:SigningKey");
+
+        services.AddScoped<IJwtTokenGenerator>(config => new JwtTokenGenerator(expirationTimeMinutes, signingKey!));
+    }
+
+    private static void AddDBContext(IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<LingoForgeDbContext>(x => x.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+    }
+
+    private static void AddRepositories(IServiceCollection services)
+    {
+        services.AddScoped<IUnityOfWork, UnityOfWork>();
+    }
+}
--- LingoForge/Extensions/ServiceCollectionExtensions.cs
+++ LingoForge/Extensions/ServiceCollectionExtensions.cs
@@ -1,4 +1,16 @@
-﻿using Microsoft.OpenApi.Models;
+﻿using LingoForge.Domain.DTOs.Responses;
+using LingoForge.Domain.Enums;
+using LingoForge.Domain.Security;
+using LingoForge.Handlers;
+using LingoForge.Application;
+using LingoForge.Infrastructure;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Json;
 
 namespace LingoForge.Extensions;
 
@@ -7,7 +19,10 @@ public static class ServiceCollectionExtensions
     public static IServiceCollection AddCustomServices(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddControllers();
+        services.AddInfrastructure(configuration);
+        services.AddApplication();
         services.AddRouting(options => options.LowercaseUrls = true);
+        services.AddSingleton<IAuthorizationMiddlewareResultHandler, AuthorizationResultHandler>();
 
         return services;
     }
@@ -52,14 +67,64 @@ public static class ServiceCollectionExtensions
 
     public static IServiceCollection AddAuthentication(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddAuthentication();
+        var signingKey = configuration.GetValue<string>("Settings:Jwt:SigningKey");
+
+        services.AddAuthentication(config =>
+        {
+            config.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            config.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(config =>
+        {
+            config.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ClockSkew = new TimeSpan(0),
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(signingKey!)),
+                RoleClaimType = ClaimTypes.Role,
+                NameClaimType = ClaimTypes.Name,
+            };
+
+            config.Events = new JwtBearerEvents
+            {
+                OnAuthenticationFailed = context =>
+                {
+                    context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                    context.Response.ContentType = "application/json";
+                    var errorResponse = new BaseResponseErrorDTO("Seu token é inválido ou expirou.");
+                    return context.Response.WriteAsync(JsonSerializer.Serialize(errorResponse));
+                },
+
+                OnChallenge = context =>
+                {
+                    context.HandleResponse();
+
+                    if (!context.Response.HasStarted)
+                    {
+                        context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                        context.Response.ContentType = "application/json";
+                        var errorResponse = new BaseResponseErrorDTO("É necessário uma autenticação para acessar este recurso.");
+                        return context.Response.WriteAsync(JsonSerializer.Serialize(errorResponse));
+                    }
+
+                    return Task.CompletedTask;
+                }
+            };
+        });
 
         return services;
     }
 
     public static IServiceCollection AddCustomAuthorization(this IServiceCollection services)
     {
-        services.AddAuthorization();
+        services.AddAuthorization(options =>
+        {
+            options.AddPolicy(AuthorizationPolicies.MustBeStudent, policy =>
+                policy.RequireRole(nameof(EUserRole.STUDENT)));
+
+            options.AddPolicy(AuthorizationPolicies.MustBeTeacher, policy =>
+                policy.RequireRole(nameof(EUserRole.TEACHER)));
+        });
 
         return services;
     }
