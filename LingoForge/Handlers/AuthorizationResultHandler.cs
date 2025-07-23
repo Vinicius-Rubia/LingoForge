d0ba1c108da09905199708ac728460d6db05e6e3
Vinicius-Rubia <lucas.de.oliveira.rubia1@gmail.com>
2025-07-23 20:31:54 -0300

    feat: adicionar handler para capturar o momento em que o usuário não tem permissão para executar a ação deseja por conta do nível de acesso

---
+++ LingoForge/Handlers/AuthorizationResultHandler.cs
@@ -0,0 +1,32 @@
+﻿using LingoForge.Domain.DTOs.Responses;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Policy;
+using System.Text.Json;
+
+namespace LingoForge.Handlers;
+
+public class AuthorizationResultHandler : IAuthorizationMiddlewareResultHandler
+{
+    private readonly AuthorizationMiddlewareResultHandler _defaultHandler;
+
+    public AuthorizationResultHandler()
+    {
+        _defaultHandler = new AuthorizationMiddlewareResultHandler();
+    }
+
+    public async Task HandleAsync(RequestDelegate next, HttpContext context, AuthorizationPolicy policy, PolicyAuthorizationResult authorizeResult)
+    {
+        if (authorizeResult.Forbidden)
+        {
+            context.Response.StatusCode = StatusCodes.Status403Forbidden;
+            context.Response.ContentType = "application/json";
+
+            var errorResponse = new BaseResponseErrorDTO("Seu nível de acesso não pode realizar esta ação.");
+
+            await context.Response.WriteAsync(JsonSerializer.Serialize(errorResponse));
+            return;
+        }
+
+        await _defaultHandler.HandleAsync(next, context, policy, authorizeResult);
+    }
+}
