1f3dc5d85d96b0321221c90f1af25e3004044a52
Vinicius-Rubia <lucas.de.oliveira.rubia1@gmail.com>
2025-07-28 19:45:07 -0300

    feat: adicionar controller para cadastrar um novo usuário

---
+++ LingoForge/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using LingoForge.Domain.DTOs.Requests;
+using LingoForge.Domain.DTOs.Responses;
+using LingoForge.Domain.Interfaces.UseCases.Auth;
+using LingoForge.Domain.Security;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LingoForge.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+[Authorize]
+
+public class UserController : ControllerBase
+{
+    [HttpPost("create-account-for-student")]
+    [Authorize(Policy = AuthorizationPolicies.MustBeTeacher)]
+    [ProducesResponseType(typeof(ResponseCreatedUserDTO), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(BaseResponseErrorDTO), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(BaseResponseErrorDTO), StatusCodes.Status403Forbidden)]
+    public async Task<IActionResult> PromoteToTeacher([FromServices] ICreateAccountUseCase useCase, [FromBody] RequestCreateAccountDTO request)
+    {
+        var response = await useCase.Execute(request);
+        return Created(string.Empty, response);
+    }
+}
