af79203cf0dc8ccf18edbd3837e60eabd1a1724c
Vinicius-Rubia <lucas.de.oliveira.rubia1@gmail.com>
2025-07-28 19:45:24 -0300

    feat: adicionar mapping para entidade usuário

---
+++ LingoForge.Infrastructure/DataAccess/Mappings/BaseMapping.cs
@@ -0,0 +1,19 @@
+﻿using LingoForge.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LingoForge.Infrastructure.DataAccess.Mappings;
+
+internal class BaseMapping<TEntity> : IEntityTypeConfiguration<TEntity> where TEntity : BaseEntity
+{
+    public virtual void Configure(EntityTypeBuilder<TEntity> builder)
+    {
+        builder.Property(x => x.CreatedAt)
+            .IsRequired()
+            .HasDefaultValueSql("now()");
+
+        builder.Property(x => x.UpdatedAt)
+            .IsRequired()
+            .HasDefaultValueSql("now()");
+    }
+}
---
+++ LingoForge.Infrastructure/DataAccess/Mappings/UserMapping.cs
@@ -0,0 +1,35 @@
+﻿using LingoForge.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LingoForge.Infrastructure.DataAccess.Mappings;
+
+internal class UserMapping : BaseMapping<User>
+{
+    public override void Configure(EntityTypeBuilder<User> builder)
+    {
+        base.Configure(builder);
+        builder.ToTable("users");
+
+        builder.Property(x => x.Name)
+            .IsRequired()
+            .HasColumnType("VARCHAR")
+            .HasMaxLength(20);
+
+        builder.Property(x => x.Email)
+            .IsRequired()
+            .HasColumnType("VARCHAR")
+            .HasMaxLength(255);
+
+        builder.HasIndex(x => x.Email)
+           .IsUnique();
+
+        builder.Property(x => x.Password)
+            .IsRequired()
+            .HasColumnType("CHAR(60)");
+
+        builder.Property(x => x.Role)
+           .IsRequired()
+           .HasColumnType("INT");
+    }
+}
