586cf57777d54c249d4605811d9514c1f2a6c822
Vinicius-Rubia <lucas.de.oliveira.rubia1@gmail.com>
2025-07-28 19:45:38 -0300

    feat: adicionar migration para tabela usuário

---
+++ LingoForge.Infrastructure/Migrations/20250728221445_CreateTableUser.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LingoForge.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateTableUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "VARCHAR", maxLength: 20, nullable: false),
+                    Email = table.Column<string>(type: "VARCHAR", maxLength: 255, nullable: false),
+                    Password = table.Column<string>(type: "CHAR(60)", nullable: false),
+                    Role = table.Column<int>(type: "INT", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "now()"),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "now()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_Email",
+                table: "users",
+                column: "Email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
