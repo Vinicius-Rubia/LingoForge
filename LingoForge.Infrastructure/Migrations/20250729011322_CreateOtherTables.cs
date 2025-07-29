7872a7005b6ca94a9147921ee6c67307da82231a
Vinicius-Rubia <lucas.de.oliveira.rubia1@gmail.com>
2025-07-28 22:20:41 -0300

    feat: adicionar migrations das demais tabelas

---
+++ LingoForge.Infrastructure/Migrations/20250729011322_CreateOtherTables.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LingoForge.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateOtherTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "classes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "VARCHAR", maxLength: 20, nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    TeacherId = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "now()"),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "now()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_classes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_classes_users_TeacherId",
+                        column: x => x.TeacherId,
+                        principalTable: "users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "activities",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "VARCHAR", maxLength: 20, nullable: false),
+                    Instructions = table.Column<string>(type: "TEXT", nullable: true),
+                    Type = table.Column<int>(type: "INT", nullable: false),
+                    TurmaId = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "now()"),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "now()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_activities", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_activities_classes_TurmaId",
+                        column: x => x.TurmaId,
+                        principalTable: "classes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "class_enrollments",
+                columns: table => new
+                {
+                    TurmaId = table.Column<Guid>(type: "uuid", nullable: false),
+                    StudentId = table.Column<Guid>(type: "uuid", nullable: false),
+                    EnrolledAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "now()"),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "now()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_class_enrollments", x => new { x.TurmaId, x.StudentId });
+                    table.ForeignKey(
+                        name: "FK_class_enrollments_classes_TurmaId",
+                        column: x => x.TurmaId,
+                        principalTable: "classes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_class_enrollments_users_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "answers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ActivityId = table.Column<Guid>(type: "uuid", nullable: false),
+                    StudentId = table.Column<Guid>(type: "uuid", nullable: false),
+                    FreeTextContent = table.Column<string>(type: "TEXT", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "now()"),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "now()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_answers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_answers_activities_ActivityId",
+                        column: x => x.ActivityId,
+                        principalTable: "activities",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_answers_users_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "questions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ActivityId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Statement = table.Column<string>(type: "TEXT", nullable: false),
+                    Order = table.Column<int>(type: "INT", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "now()"),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "now()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_questions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_questions_activities_ActivityId",
+                        column: x => x.ActivityId,
+                        principalTable: "activities",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "answer_items",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    AnswerId = table.Column<Guid>(type: "uuid", nullable: false),
+                    QuestionId = table.Column<Guid>(type: "uuid", nullable: false),
+                    AnswerText = table.Column<string>(type: "TEXT", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "now()"),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "now()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_answer_items", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_answer_items_answers_AnswerId",
+                        column: x => x.AnswerId,
+                        principalTable: "answers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_answer_items_questions_QuestionId",
+                        column: x => x.QuestionId,
+                        principalTable: "questions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_activities_TurmaId",
+                table: "activities",
+                column: "TurmaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_answer_items_AnswerId",
+                table: "answer_items",
+                column: "AnswerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_answer_items_QuestionId",
+                table: "answer_items",
+                column: "QuestionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_answers_ActivityId",
+                table: "answers",
+                column: "ActivityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_answers_StudentId",
+                table: "answers",
+                column: "StudentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_class_enrollments_StudentId",
+                table: "class_enrollments",
+                column: "StudentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_classes_TeacherId",
+                table: "classes",
+                column: "TeacherId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_questions_ActivityId",
+                table: "questions",
+                column: "ActivityId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "answer_items");
+
+            migrationBuilder.DropTable(
+                name: "class_enrollments");
+
+            migrationBuilder.DropTable(
+                name: "answers");
+
+            migrationBuilder.DropTable(
+                name: "questions");
+
+            migrationBuilder.DropTable(
+                name: "activities");
+
+            migrationBuilder.DropTable(
+                name: "classes");
+        }
+    }
+}
